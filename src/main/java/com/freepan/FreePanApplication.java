1c2f8095374050cd5a94e52f174278683d3c6651
Alicecici <2149382799@qq.com>
2025-03-11 18:06:47 +0800

    管理员

---
+++ src/main/java/com/freepan/FreePanApplication.java
@@ -0,0 +1,38 @@
+package com.freepan;
+
+import com.freepan.entity.config.AppConfig;
+import com.freepan.entity.constants.Constants;
+import com.freepan.spring.ApplicationContextProvider;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.servlet.MultipartConfigElement;
+
+
+@EnableAsync
+@SpringBootApplication(scanBasePackages = {"com.freepan"})
+@MapperScan(basePackages = {"com.freepan.mappers"})
+@EnableTransactionManagement
+@EnableScheduling
+public class FreePanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreePanApplication.class, args);
+    }
+
+    @Bean
+    @DependsOn({"applicationContextProvider"})
+    MultipartConfigElement multipartConfigElement() {
+        AppConfig appConfig = (AppConfig) ApplicationContextProvider.getBean("appConfig");
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setLocation(appConfig.getProjectFolder() + Constants.FILE_FOLDER_TEMP);
+        return factory.createMultipartConfig();
+    }
+}
